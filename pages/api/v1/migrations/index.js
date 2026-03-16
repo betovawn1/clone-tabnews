ea089235602113727cf3379f90c88da293d99fc5
decobot <capy@deco.cx>
2026-03-16 10:14:53 -0300

    Nova api coberta por testes que executa migrations

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,36 @@
+import migrationRunner from 'node-pg-migrate';
+import { join } from 'node:path';
+
+async function migrations(request, response) {
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+    migrationsTable: 'pgmigrations'
+  };
+
+  if (request.method === 'GET') {
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: true
+    });
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === 'POST') {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false
+    });
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
+
+export default migrations;
