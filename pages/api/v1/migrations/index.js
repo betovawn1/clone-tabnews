bdec437ba22f660bc4edd800b524de8ea0abcdc7
betovawn <sergioluizfrancajr@gmail.com>
2026-08-01 16:32:03 -0300

    fix: garante que todas as conexões abertas contra o banco ao rodar migrations seja encerrada ao final

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -14,28 +14,29 @@ async function migrations(request, response) {
     migrationsTable: 'pgmigrations'
   };
 
-  if (request.method === 'GET') {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false
-    });
-
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+  try {
+    if (request.method === 'GET') {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+    if (request.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false
+      });
+  
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+  
+      return response.status(200).json(migratedMigrations);
+    }
 
-  return response.status(405).end();
+    return response.status(405).end();
+  } finally {
+    await dbClient.end();
+  }
 }
 
 export default migrations;
